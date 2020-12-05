3f6d97f9b2d4b3effb68dbd79934193be8e73283
jhon <jhon.carrasco@gmail.com>
2020-12-05 19:54:41 -0300

    MONGO_URI environment variable

--- server/config/config.js
+++ server/config/config.js
@@ -19,6 +19,7 @@ let urlDB
 if ( process.env.NODE_ENV === 'dev' )
     urlDB = 'mongodb://localhost:27017/cafe'
 else
-    urlDB = 'mongodb+srv://admin:Cafe.2020@cluster0.h0btw.mongodb.net/cafe'
+    // urlDB = 'mongodb+srv://admin:Cafe.2020@cluster0.h0btw.mongodb.net/cafe'
+    urlDB = process.env.MONGO_URI
 
 process.env.MONGO_URI = urlDB
